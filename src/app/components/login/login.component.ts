43cc9d94764947e9908a008e4326c1860c626c02
CarmineC93 <carmine.caglia93@gmail.com>
2023-05-31 18:45:08 +0200

    add: register component form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(form:NgForm){
+    const email = form.value.email;
+    const password = form.value.password;
+  }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  constructor(){}
 
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(form:NgForm){
+    const email = form.value.email;
+    const password = form.value.password;
+  }
 }
