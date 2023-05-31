953833ece794971cfd440108511acc3af7c2bb7a
CarmineC93 <carmine.caglia93@gmail.com>
2023-05-31 17:10:14 +0200

    installed angular material indico/pink, global typo styles, include animation model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,21 @@ import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
